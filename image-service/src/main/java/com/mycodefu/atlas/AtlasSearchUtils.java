a57d603ba82e00a6cb84aef6ab1df0f603ddf293
Luke Thompson <endeavour9@gmail.com>
2024-07-13 21:27:24 +1000

    remove dead code

--- image-service/src/main/java/com/mycodefu/atlas/AtlasSearchUtils.java
+++ image-service/src/main/java/com/mycodefu/atlas/AtlasSearchUtils.java
@@ -1,8 +1,6 @@
 package com.mycodefu.atlas;
 
 import com.mongodb.client.model.search.SearchOperator;
-import com.mongodb.client.model.search.SearchPath;
-import com.mongodb.client.model.search.TextSearchOperator;
 import org.bson.BsonDateTime;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -10,7 +8,6 @@ import org.bson.conversions.Bson;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class AtlasSearchUtils {
 
@@ -61,74 +58,6 @@ public class AtlasSearchUtils {
             rangeDocument.append("lte", to);
         }
         return new Document("range", rangeDocument);
-    }
-
-    public static SearchOperator queryStringOrWildcard(String key, String value) {
-        SearchOperator search;
-        String cleanValue = cleanWordForSearch(value);
-        if (isWord(cleanValue)) {
-            search = wildcard(key, cleanValue);
-        } else {
-            search = queryString(key, "\"" + cleanValue + "\"");
-        }
-        return SearchOperator.of(search);
-    }
-
-    public static Bson queryForList(String path, List<String> values) {
-        SearchPath searchPath = SearchPath.fieldPath(path).multi("keywordAnalyzer");
-        List<TextSearchOperator> shouldList = values.stream()
-                .map(value -> SearchOperator.text(searchPath, value)).toList();
-        return SearchOperator.compound().should(shouldList);
-    }
-
-    public static SearchOperator queryString(String key, String query) {
-        Document queryStringDocument = new Document("queryString", new Document()
-                .append("defaultPath", key)
-                .append("query", query)
-        );
-        return SearchOperator.of(queryStringDocument);
-    }
-
-    public static SearchOperator wildcard(String key, String value) {
-        String valueWithoutAsterix = value;
-        if (value.startsWith("*")) {
-            valueWithoutAsterix = value.substring(1);
-        }
-        if (value.endsWith("*")) {
-            valueWithoutAsterix = valueWithoutAsterix.substring(0, valueWithoutAsterix.length() - 1);
-        }
-
-        return SearchOperator.of(new Document("wildcard", new Document()
-                .append("path", key)
-                .append("query", "*" + cleanWordForSearch(valueWithoutAsterix) + "*")
-                .append("allowAnalyzedField", true)
-        ));
-    }
-
-    static final Pattern WORD_PATTERN = Pattern.compile("^[a-zA-Z0-9_]+$");
-    public static boolean isWord(String value) {
-        return WORD_PATTERN.matcher(value).matches();
-    }
-
-    public static boolean isSpecialChar(char c) {
-        char[] queryChars = new char[]{'+', '-', '&', '|', '{', '}', '!', '(', ')', '\"', '~', '*', '?', '\\'};
-        for (char queryChar : queryChars) {
-            if (c == queryChar) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static String cleanWordForSearch(String text) {
-        StringBuilder cleanedText = new StringBuilder();
-        for (char c : text.toCharArray()) {
-            if (isSpecialChar(c)) {
-                cleanedText.append('\\');
-            }
-            cleanedText.append(Character.toLowerCase(c));
-        }
-        return cleanedText.toString().trim();
     }
 
     public static <T> SearchOperator mustClauseForCollection(String path, Collection<T> values) {
