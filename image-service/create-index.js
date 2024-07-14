5ef000813d719b757dca48582d884524fe672c03
Luke Thompson <endeavour9@gmail.com>
2024-07-14 18:25:07 +1000

    add index for new info fields

--- image-service/create-index.js
+++ image-service/create-index.js
@@ -1,4 +1,40 @@
-db.getSiblingDB('ImageSearch').getCollection('photos').createSearchIndex('default',
+//aggregate two collections together photos and photoinfo based on a $lookup using filename
+db.getSiblingDB('ImageSearch').getCollection('photos').aggregate([
+    {
+        $lookup: {
+            from: 'photoinfo',
+            localField: 'filename',
+            foreignField: 'filename',
+            as: 'info'
+        }
+    },
+    {
+        $unwind: {
+            path: '$info',
+            preserveNullAndEmptyArrays: true
+        }
+    },
+    {
+        $project: {
+            _id: 1,
+            url: '$info.url',
+            caption: '$caption',
+            summary: '$info.info.detailedCaption',
+            hasPerson: '$info.info.hasPerson',
+            dogs: '$info.info.dogs',
+            runData: {
+                filename: '$filename',
+                captionTimeTakenSeconds: '$caption_time_seconds',
+                infoTimeTakenSeconds: '$info.time_taken_seconds',
+            }
+        }
+    },
+    {
+        $out: 'photo'
+    }
+]);
+//create search index on photo collection
+db.getSiblingDB('ImageSearch').getCollection('photo').createSearchIndex('default',
     {
         "mappings": {
             "fields": {
@@ -8,9 +44,49 @@ db.getSiblingDB('ImageSearch').getCollection('photos').createSearchIndex('defaul
                     },
                     {
                         "type": "autocomplete"
-
                     }
-                ]
+                ],
+                "summary": [
+                    {
+                        "type": "string"
+                    }
+                ],
+                "hasPerson": {
+                    "type": "boolean"
+                },
+                "dogs": {
+                    "type": "embeddedDocuments",
+                    "dynamic": false,
+                    "fields": {
+                        "colour": {
+                            "type": "string",
+                            "multi": {
+                                "keywordAnalyzer": {
+                                    "type": "string",
+                                    "analyzer": "lucene.keyword"
+                                }
+                            }
+                        },
+                        "size": {
+                            "type": "string",
+                            "multi": {
+                                "keywordAnalyzer": {
+                                    "type": "string",
+                                    "analyzer": "lucene.keyword"
+                                }
+                            }
+                        },
+                        "breed": {
+                            "type": "string",
+                            "multi": {
+                                "keywordAnalyzer": {
+                                    "type": "string",
+                                    "analyzer": "lucene.keyword"
+                                }
+                            }
+                        }
+                    }
+                },
             }
         }
     }
--- image-service/import-data.sh
+++ image-service/import-data.sh
@@ -1,4 +1,6 @@
 # convert output of machine learning model to JSON Lines (jsonl)
 cat ../image-data/photo-results.json | jq .photos | tr -d '\n' | jq -c '.[]' > data.jsonl
+cat ../image-data/photo-results-info-temp.json | jq .photos | tr -d '\n' | jq -c '.[]' > data-info.jsonl
 # import jsonl to mongo, dropping existing collection
 mongoimport --db ImageSearch --collection photos --drop --file data.jsonl
+mongoimport --db ImageSearch --collection photoinfo --drop --file data-info.jsonl
