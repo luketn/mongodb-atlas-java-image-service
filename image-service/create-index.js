44a4d6a043bca50ffe2a428db2769b72ac075d6c
Luke Thompson <endeavour9@gmail.com>
2024-07-17 21:26:02 +1000

    fix bug in create index script (newlines being stripped meant all commented)

--- image-service/create-index.js
+++ image-service/create-index.js
@@ -1,6 +1,4 @@
-//create search index on photo collection
 db.getSiblingDB('ImageSearch').getCollection('photo').dropSearchIndex('default');
-
 db.getSiblingDB('ImageSearch').getCollection('photo').createSearchIndex('default',
 INDEX_GOES_HERE
 );
